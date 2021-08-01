605bd89674dea94d50bede32611007066659670c
Derek Beckford <dbeck1207@icloud.com>
2021-08-01 18:43:13 -0500

    logic to category and product route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -28,34 +28,77 @@ router.get("/:id", (req, res) => {
     },
     attributes: ['id', 'category_name'],
     include: [
+      // be sure to include its associated Products
       {
         model: Product,
         attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
       }
     ]
   })
-  .then(dbCategoryData) {
+  .then(dbCategoryData => {
+    if(!dbCategoryData){
     res.status(404).json({message: 'No category found with his id'});
     return;
-  }
+    }
+    res.json(dbCategoryData);
+  })
   .catch (err =>{
     console.log(err);
     res.status(500).json(err);
   })
 
-  // be sure to include its associated Products
+  
 });
 
 router.post("/", (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if(!dbCategoryData[0]){
+    res.status(404).json({message: 'No category found with his id'});
+    return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch (err =>{
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if(!dbCategoryData){
+    res.status(404).json({message: 'No category found with his id'});
+    return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch (err =>{
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
